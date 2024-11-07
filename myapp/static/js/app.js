04f2075995bda66528332094f8de98da6ad2e101
DAUDI H RAMADHANI <daudiramadhani79@gmail.com>
2024-11-07 17:03:30 +0300

    initial

---
+++ myapp/static/js/app.js
@@ -0,0 +1,20 @@
+import './bootstrap';
+import './jquery';
+import './bootstrap.min';
+import './bootsnav';
+import './jquery.filterizr.min';
+import './jquery-ui.min';
+import './jquery.counterup.min';
+import './waypoints.min';
+import './owl.carousel.min';
+import './jquery.sticky';
+import './datepicker';
+import './custom';
+
+
+
+
+
+
+
+
